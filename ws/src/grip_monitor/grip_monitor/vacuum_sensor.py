944baf805ad92670f3962b8e94cb6b3026855e96
Maniumek <gugnowski@gmail.com>
2026-09-05 16:35:37 +0200

    devcontainer: terminal jako powloka logowaniowa, inaczej pomija /etc/profile.d

--- ws/src/grip_monitor/grip_monitor/vacuum_sensor.py
+++ ws/src/grip_monitor/grip_monitor/vacuum_sensor.py
@@ -1,3 +1,18 @@
-import rcply
+from rclpy.node import Node
+from rclpy import init, spin, shutdown
 
-class VacuumSensor: rcply.
+class VacuumSensor(Node):
+    def __init__(self):
+        super().__init__('vacuum_sensor')
+
+def main(args=None):
+    init()
+
+    node = VacuumSensor()
+    spin(node)
+    node.destroy_node()
+
+    shutdown()
+
+if __name__ == '__main__':
+    main()
--- ws/src/grip_monitor/setup.py
+++ ws/src/grip_monitor/setup.py
@@ -24,6 +24,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'vacuum_sensor = grip_monitor.vacuum_sensor:main'
         ],
     },
 )
