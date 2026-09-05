463b0fbc8ef489250a77d1747cc702ad2034e45f
Maniumek <gugnowski@gmail.com>
2026-09-05 17:40:49 +0200

    vacuumSensorNode-publishes-at-50hz

--- ws/src/grip_monitor/grip_monitor/vacuum_sensor.py
+++ ws/src/grip_monitor/grip_monitor/vacuum_sensor.py
@@ -1,12 +1,23 @@
 from rclpy.node import Node
 from rclpy import init, spin, shutdown
+from std_msgs.msg import Float32
+from rcl_interfaces.msg._parameter_event import ParameterEvent
 
 class VacuumSensor(Node):
     def __init__(self):
         super().__init__('vacuum_sensor')
+        self.pub = self.create_publisher(Float32, 'vacuum_pressure', 10)
+        self.create_timer(1.0/50, self.tick)
+
+    def tick(self):
+        msg = Float32()
+        msg.data = -58.0
+
+        self.pub.publish(msg)
+
 
 def main(args=None):
-    init()
+    init(args=args)
 
     node = VacuumSensor()
     spin(node)
